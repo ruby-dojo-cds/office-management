824db22b18b217d63c73326134e94cc1331a8eaa
Jonathan Javiel <jjaviel@codigodelsur.com>
2019-06-27 12:35:29 -0300

    detalle en correcion rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 
-class ActiveSupport
+module ActiveSupport
   class TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
